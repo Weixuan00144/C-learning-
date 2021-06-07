358f741bdf770f617bfcc59eaddeb581409336dd
Weixuan Cao <cao00144@umn.edu>
2021-06-07 16:15:25 -0500

    without &

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void getInput (double &  fVariable)
+void getInput (double   fVariable)
 { using namespace std;
 cout << "I will convert a Fahrenheit temperature"
 << " to Celsius.\n"
